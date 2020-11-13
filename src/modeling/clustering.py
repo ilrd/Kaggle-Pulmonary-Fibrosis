51e5a2ddc8b2957039f32ee4f45949da50db84eb
Illia Herasymenko <illia.cgerasimenko@gmail.com>
2020-11-13 10:33:27 +0200

    Added comments, better code organization

--- src/modeling/clustering.py
+++ src/modeling/clustering.py
@@ -6,55 +6,62 @@ import os
 from models import clustering_autoencoder
 from tensorflow.keras import callbacks
 import datetime
+from random import randint
 
-np.random.seed(4)
+# Setting seed
+SEED = 4
+np.random.seed(SEED)
 
 gpus = tf.config.experimental.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(gpus[0], True)
 
-window = [-100, 155]
-
 
 # ==================================#
+# Data generator
+def get_data(ids=None, window=None, ids_shuffle=False, iterate=True, seed=None, batch_size=1):
+    data_dir = '../../data/processed/train'
 
-def get_data(nums=None, window=None, shuffle=True, full_shuffle=False, names=False, iter=True):
-    data_dir = '../../data/clustering/train'
+    patient_ids = np.array(os.listdir(data_dir))
 
-    all_images = np.array(os.listdir(data_dir))
-    if full_shuffle:
-        np.random.shuffle(all_images)
+    if ids_shuffle:
+        if seed:
+            np.random.seed(seed)
+        else:
+            np.random.seed(randint(5, 500))
+        np.random.shuffle(patient_ids)
 
-    if nums:
-        img_names = all_images[nums[0]:nums[1]]
-    else:
-        img_names = all_images
+    if ids:
+        patient_ids = patient_ids[ids[0]:ids[1] - 1]
 
+    batch = []
     while True:
-        if shuffle:
-            np.random.shuffle(img_names)
-        for j, dcm_img_name in enumerate(img_names, start=1):
+        for patient_id in patient_ids:
+            img_names = os.listdir(os.path.join(data_dir, patient_id))
 
-            dcm_img_path = os.path.join(data_dir, dcm_img_name)
-            dcm_img_array = np.load(dcm_img_path)
-            if window:
-                lb = window[0]
-                ub = window[1]
-                dcm_img_array[dcm_img_array < lb] = lb
-                dcm_img_array[dcm_img_array > ub] = ub
-                dcm_img_array = (dcm_img_array - lb) / (ub - lb)
+            for j, img_name in enumerate(img_names, start=1):
+                img_path = os.path.join(data_dir, patient_id, img_name)
+                img_array = np.load(img_path)
 
-            dcm_img_array = np.reshape(dcm_img_array, newshape=(1, 512, 512, 1))
-            if not names:
-                yield dcm_img_array
-            else:
-                yield dcm_img_array, dcm_img_name
+                if window:
+                    lb = window[0]
+                    ub = window[1]
+                    img_array[img_array < lb] = lb
+                    img_array[img_array > ub] = ub
+                    img_array = (img_array - lb) / (ub - lb)
 
-        if not iter:
+                img_array = np.reshape(img_array, newshape=(512, 512, 1))
+                batch.append(img_array)
+
+                if len(batch) == batch_size:
+                    yield np.array(batch)
+                    batch = []
+
+        if not iterate:
             break
 
 
 # ==================================#
-
+# Custom callback
 class BatchLoss(callbacks.Callback):
     def on_train_begin(self, logs=None):
         self.model.history.history['batch_loss'] = []
@@ -64,41 +71,34 @@ class BatchLoss(callbacks.Callback):
         self.model.history.history['batch_loss'].append(batch_loss)
 
 
-class RestoreDefaults(callbacks.Callback):
-    def on_train_begin(self, logs=None):
-        default_lr = 0.001
-        tf.keras.backend.set_value(self.model.optimizer.lr, default_lr)
-
-
 # ==================================#
 
-def fit_model(model, train_gen, val_gen, samples, epochs=5, train_callbacks=None):
-    steps_per_epoch = samples[1] - samples[0]
+def fit_model(model, train_gen, val_gen, batch_size=1, epochs=5, train_callbacks=None):
     train_gen = ((train_sample, train_sample) for train_sample in train_gen)
     val_gen = ((val_sample, val_sample) for val_sample in val_gen)
     history = model.fit(train_gen, batch_size=1, epochs=epochs, validation_data=val_gen,
-                        steps_per_epoch=steps_per_epoch, callbacks=train_callbacks,
-                        validation_steps=5)
+                        steps_per_epoch=130 * 15 // batch_size, callbacks=train_callbacks,
+                        validation_steps=20 * 15 // batch_size)
     return history
 
 
 # ==================================#
-# Training, testing
+# Training
 
-def training_loop(autoencoder):
-    train_samples = [0, 100]
-    val_samples = [0, 2000]
+def training_loop(autoencoder, window):
+    train_samples = [0, 130]
+    val_samples = [130, 150]
     log_dir = "logs/clustering_fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    checkpoint_filepath = f'saved_models/clustering/clustering_checkpoint3.h5'
+    checkpoint_filepath = f'./saved_models/clustering_models/clustering_checkpoint.h5'
 
     train_callbacks = [
         callbacks.ReduceLROnPlateau(
             monitor='val_loss',
             factor=0.3,
-            patience=3,
+            patience=2,
             min_delta=0.01,
             cooldown=0,
-            min_lr=1e-6,
+            min_lr=1e-5,
             verbose=1,
         ),
         callbacks.ModelCheckpoint(
@@ -114,90 +114,77 @@ def training_loop(autoencoder):
         BatchLoss(),
     ]
 
-    gen = get_data(nums=train_samples, window=window, full_shuffle=True)
-    val_gen = get_data(nums=val_samples, window=window, full_shuffle=True)
+    batch_size = 5
+    train_gen = get_data(ids=train_samples, window=window, ids_shuffle=True, seed=SEED, batch_size=batch_size)
+    val_gen = get_data(ids=val_samples, window=window, ids_shuffle=True, seed=SEED, batch_size=batch_size)
 
-    train_history = fit_model(autoencoder, gen, val_gen, train_samples, 15, train_callbacks).history
+    train_history = fit_model(autoencoder, train_gen, val_gen, batch_size, 10, train_callbacks).history
 
     return train_history
 
 
-autoencoder, encoder, decoder = clustering_autoencoder()
-training_loop(autoencoder)
+window = [-100, 155]
+
+
+# autoencoder = clustering_autoencoder()
+# full_train_history = training_loop(autoencoder, window=window)
+
+# ==================================#
+# Plot training history
+def plot_history(history):
+    plt.figure()
+    plt.plot(history['batch_loss'][:])
+
+    plt.figure()
+    plt.plot(history['val_loss'][:], label='val_loss')
+    plt.plot(history['loss'][:], label='loss')
+    plt.legend()
+    plt.show()
 
 
 # ==================================#
 
-def plot_example():
-    gen = get_data(nums=[2000, 3000], window=window, full_shuffle=True)
+# Plot example of encoded-decoded image along with the original
+def plot_example(autoencoder, window):
+    gen = get_data(ids=[150, 175], window=window, ids_shuffle=True, batch_size=1, iterate=False)
 
-    x1 = next(gen)
+    x = next(gen)
 
-    # autoencoder = keras.models.load_model('modeling/saved_models/clustering/clustering_checkpoint3.h5')
-    #
-    # x1 = np.reshape(x1, newshape=(1, 512, 512, 1))
-    #
-    # decoded1 = autoencoder.predict(x1)
-    # x1 = np.reshape(x1, newshape=(512, 512))
-    # decoded1 = np.reshape(decoded1, newshape=(512, 512))
-    # plt.figure()
-    # plt.subplot(121)
-    # plt.imshow(x1, plt.cm.bone)
-    # plt.subplot(122)
-    # plt.imshow(decoded1, plt.cm.bone)
+    x = np.reshape(x, newshape=(1, 512, 512, 1))
 
-    autoencoder = keras.models.load_model('modeling/saved_models/clustering/best_clustering_autoencoder.h5')
-
-    x1 = np.reshape(x1, newshape=(1, 512, 512, 1))
-
-    decoded1 = autoencoder.predict(x1)
-    x1 = np.reshape(x1, newshape=(512, 512))
+    decoded1 = autoencoder.predict(x)
+    x = np.reshape(x, newshape=(512, 512))
     decoded1 = np.reshape(decoded1, newshape=(512, 512))
     plt.figure()
     plt.subplot(121)
-    plt.imshow(x1, plt.cm.bone)
+    plt.imshow(x, plt.cm.bone)
     plt.subplot(122)
     plt.imshow(decoded1, plt.cm.bone)
 
 
-plot_example()
-plot_example()
+# plot_example(autoencoder, window=window)
 
 
 # ==================================#
-
-# plt.figure()
-# plt.plot(full_train_history[0]['batch_loss'][:])
-#
-# plt.figure()
-# plt.plot(full_train_history[0]['val_loss'][:], label='val_loss')
-# plt.plot(full_train_history[0]['loss'][:], label='loss')
-
-
-# ==================================#
-# K-means
-autoencoder = keras.models.load_model('modeling/saved_models/clustering/clustering_checkpoint3.h5')
+# K-means clustering based on encoded vector of the images
+autoencoder = keras.models.load_model('./saved_models/clustering_models/clustering_15ep_1024dim.h5')
 encoder = autoencoder.layers[1]
-preproc_gen = get_data(nums=[0, 1], window=window, shuffle=False, names=False, iter=False)
-enc1 = encoder.predict(preproc_gen)
+preproc_gen = get_data(ids=[0, 90], window=window, iterate=False)
 from sklearn.cluster import KMeans
-
-nums = [0, 500]
-preproc_gen = get_data(nums=nums, window=window, shuffle=False, names=False, iter=False)
 
 scores = []
 
 X = encoder.predict(preproc_gen)
 
-for n_clusters in range(10, 11):
+for n_clusters in range(5, 25):
     kmeans = KMeans(n_clusters=n_clusters)
 
     kmeans.fit(X)
     scores.append(kmeans.score(X))
     print(n_clusters, 'clusters done')
 
-# plt.figure()
-# plt.plot(scores)
+plt.figure()
+plt.plot(scores)
 # ==================================#
 
 # PCA
@@ -212,7 +199,7 @@ X_pca = pca.fit_transform(X_scaled)
 per_var = np.round(pca.explained_variance_ratio_ * 100, 1)
 labels = ['PC' + str(x) for x in range(1, len(per_var) + 1)]
 plt.figure()
-plt.bar(x=range(1, X.shape[1] + 1), height=per_var[:], tick_label=labels[:])
+plt.bar(x=range(1, 101), height=per_var[:100], tick_label=labels[:100])
 plt.ylabel('Percentage of Explained Variance')
 plt.xlabel('Principal Component')
 plt.title('Scree Plot')
@@ -223,24 +210,3 @@ pca_df = pd.DataFrame(X_pca, columns=labels)
 plt.figure()
 plt.scatter(pca_df.PC1, pca_df.PC2)
 plt.show()
-# ==================================#
-# Processing the data
-
-# train_dir = '../../data/processed/train'
-# imgs = 0
-# for (dcm_img_array, name) in preproc_gen:
-#     dcm_img_ds = np.array([dcm_img_array, np.empty(shape=(1,), dtype=np.float32)])
-#
-#     dcm_img_ds[1] = encoder.predict(dcm_img_array)
-#
-#     patient_ID, j = name[:-4].split('-')
-#
-#     if not os.path.isdir(f'{train_dir}/{patient_ID}'):
-#         os.mkdir(f'{train_dir}/{patient_ID}')
-#
-#     np.save(f'{train_dir}/{patient_ID}/{j}', dcm_img_ds)
-#     imgs += 1
-#     if imgs >= nums[1] - nums[0]:
-#         break
-
-# ==================================#
